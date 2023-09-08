cd3edbf0d7b984357a749415503d73db564a2750
Paweł <fordcrown2009@gmail.com>
2023-09-08 03:46:15 +0200

    Basic UI

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,8 +1,28 @@
+import './App.scss';
+
 const App = () => {
 	return (
-		<div className="container">
-				<h1>App</h1>
-		</div>
+		<div className="App">
+			<div className="container">
+        <div className="header">
+          <h1>✈️ Essential Flight Tools by Paweł Nosalski</h1>
+        </div>
+
+        <div className="main">
+          <div className="tabs">
+            <div className="tab active">Weather</div>
+            <div className="tab">Transition Level</div>
+          </div>
+
+        </div>
+		  </div>
+
+      {/* <div className="bottom-corners">
+        <div className="bottom-corners__left"></div>
+        <div className="bottom-corners__center"></div>
+        <div className="bottom-corners__right"></div>
+      </div> */}
+    </div>
 	);
 }
 
