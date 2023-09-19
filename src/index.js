6ed843a6b2db0843ec12058b35785d7ccee048c3
Paweł <fordcrown2009@gmail.com>
2023-09-20 00:16:54 +0200

    Adaptations for building the project to MFS

--- src/index.js
+++ src/index.js
@@ -5,10 +5,43 @@ import './index.scss';
 
 import App from './pages/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('EFTPanelContainer'));
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+var customPanel = false;
+document.addEventListener('beforeunload', function () {
+  customPanel = false;
+}, false);
+class EFTPanel extends HTMLElement {
+  constructor() {
+    super();
+    var iframe = document.querySelector("#EFTPanelContainer");
+    if (iframe) {
+      root.render(
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      );
+    }
+  }
+  connectedCallback() {
+    var self = this;
+
+    this.m_MainDisplay = document.querySelector("#MainDisplay");
+    this.m_MainDisplay.classList.add("hidden");
+
+    this.m_Footer = document.querySelector("#Footer");
+    this.m_Footer.classList.add("hidden");
+
+    var iframe = document.querySelector("#EFTPanelContainer");
+    if (iframe) {
+      root.render(
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      );
+    }
+  }
+  updateImage() {
+  }
+}
+window.customElements.define("ingamepanel-essential-flight-tool", EFTPanel);
